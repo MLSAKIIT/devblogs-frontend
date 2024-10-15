1343f2151baed0f62bd95af3730994b9f5828374
Akash Parmar <ak.parmar1442000@gmail.com>
2024-10-15 22:51:33 +0530

    feat: made navbar responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -32,18 +32,18 @@ const NavBar = () => {
 
   return (
     <nav
-      className={`fixed z-50 w-full flex justify-between items-center px-12 h-20 py-4 ${
+      className={`fixed z-50 w-full flex justify-between items-center px-4 sm:px-12 h-20 py-4 ${
         isDarkMode ? "bg-secondary text-white" : "bg-white text-black"
       }`}
     >
-      <img className="h-16" src="/assets/mlsa-logo.png" alt="Logo" />
+      <img className="h-14 sm:h-16" src="/assets/mlsa-logo.png" alt="Logo" />
 
       <div className="flex ">
         {/* Dark Mode Toggle Button */}
         <button
           onClick={toggleDarkMode}
           aria-label="theme toggler"
-          className={`flex h-10 w-11 mr-5 items-center justify-center rounded-lg border  duration-300 hover:bg-stroke sm:flex ${
+          className={`flex h-10 w-11 mr-3 sm:mr-5 items-center justify-center rounded-lg border  duration-300 hover:bg-stroke sm:flex ${
             !isDarkMode ? " border-stroke bg-white" : "bg-dark-2 border-dark-2"
           }`}
         >
@@ -54,11 +54,11 @@ const NavBar = () => {
           )}
         </button>
 
-        <button className="w-fit rounded-xl text-primary text-base border border-primary px-3 font-semibold h-10 flex items-center justify-center mr-3 duration-200 hover:opacity-80">
+        <button className="w-fit rounded-xl text-primary text-sm sm:text-base border border-primary px-2 sm:px-3 font-semibold h-10 flex items-center justify-center mr-2 sm:mr-3 duration-200 hover:opacity-80">
           Create Blogs
         </button>
 
-        <button className="w-fit rounded-xl text-white bg-primary text-base border border-primary px-3 font-semibold h-10 flex items-center justify-center duration-200 hover:opacity-80">
+        <button className="w-fit rounded-xl text-white bg-primary text-sm sm:text-base border border-primary px-2 sm:px-3 font-semibold h-10 flex items-center justify-center duration-200 hover:opacity-80">
           Login
         </button>
       </div>
