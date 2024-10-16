70e042b941fb861ab4c134b0f574c76e4489200e
NarayanRungta <141025229+NarayanRungta@users.noreply.github.com>
2024-10-16 23:12:37 +0530

    chore: optimize the image

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+  return {
+  plugins: [
+    ViteImageOptimizer({
+      jpg: {
+        quality: 100
+      }
+    })
+  ]
+}})
