5d27d643039d2a8ffe6465da120d1872ecbecce9
Arka Pal <emailtoarka@gmail.com>
2024-10-16 16:34:36 +0530

    arkapaal-TextEditor

---
+++ src/components/TextEditor.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+function MyComponent() {
+  const [value, setValue] = useState('');
+
+  return <ReactQuill theme="snow" value={value} onChange={setValue} />;
+}
