4242bcd8836ec2828af233aa962798436b9d8b1e
Aman Aakash <116112063+Amanaakash@users.noreply.github.com>
2024-10-18 16:01:24 +0530

    feature: register page created

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,23 +1,33 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
-const LoginForm = () => {
+const RegisterForm = () => {
+  const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const navigate = useNavigate();
 
-  const handleLogin = () => {
+  const handleRegister = () => {
     //auth logic here.
   };
 
   return (
-    <div className={` px-10 py-20 rounded-3xl border-2 border-gray-200`}>
+    <div className={` px-10 py-10 rounded-3xl border-2 mt-20 border-gray-200`}>
       <h1 className="text-5xl font-semibold">Welcome to Dev Blogs!</h1>
       <p className="font-medium text-lg text-gray-500 mt-4">
         Please enter your Details
       </p>
       <div className="mt-8 ">
+        <div>
+          <label className="text-lg font-medium">Username</label>
+          <input
+            value={userName}
+            className="w-full border-2 border-gray-100 roundex-xl p-4 bg-transparent rounded-sm"
+            type="text"
+            onChange={(e) => setUserName(e.target.value)}
+            placeholder="Please enter a username"
+          />
+        </div>
         <div>
           <label className="text-lg font-medium">Email</label>
           <input
@@ -38,26 +48,21 @@ const LoginForm = () => {
             placeholder="Enter your password"
           />
         </div>
-        <div>
-          <button className="font-medium text-base text-primary hover:opacity-75 transition-opacity duration-300">
-            Forgot password
-          </button>
-        </div>
         <div className="mt-8 flex flex-col gap-y-4">
           <button
-            onClick={handleLogin}
+            onClick={handleRegister}
             className="active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01]  ease-in-out transform py-4 bg-primary rounded-xl text-white font-bold text-lg hover:opacity-75 transition-opacity duration-300"
           >
-            Sign in
+            Register
           </button>
         </div>
         <div className="mt-8 flex justify-center items-center">
-          <p className="font-medium text-base"> Don&apos;t have an account?</p>
+          <p className="font-medium text-base"> Already have an account?</p>
           <button
-            onClick={() => navigate("/register")}
+            onClick={() => navigate("/login")}
             className="ml-2 font-medium text-base text-violet-500 hover:opacity-75 transition-opacity duration-300"
           >
-            Register
+            Login
           </button>
         </div>
       </div>
@@ -65,4 +70,4 @@ const LoginForm = () => {
   );
 };
 
-export default LoginForm;
+export default RegisterForm;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import RegisterForm from "../components/RegisterForm";
+
+const Register = () => {
+  return (
+    <div className=" w-full h-screen">
+      <div className="w-full flex items-center justify-center ">
+        <RegisterForm />
+      </div>
+    </div>
+  );
+};
+
+export default Register;
