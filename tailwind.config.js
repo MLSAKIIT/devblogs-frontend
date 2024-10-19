56b946a0c46335a755ea4636935e464da1c80bf3
viki-777 <vikashkr.96453@gmail.com>
2024-10-19 23:53:47 +0530

    create blog dark mode issue fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ export default {
   theme: {
     extend: {
       colors: {
+        text: "#fff", // Light mode text color
         primary: "#9D5AE3",
         secondary: "#484848",
         darkFooter: "#333333", // Dark mode footer color
