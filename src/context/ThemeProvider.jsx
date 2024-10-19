102c3915700c8e5814d7b2e0b476660e2bb79dbc
Aman Aakash <116112063+Amanaakash@users.noreply.github.com>
2024-10-19 23:41:21 +0530

    add: a separate file for ThemeProvider

---
+++ src/context/ThemeProvider.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { ThemeContext } from "../App";
+
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
