3be93fd383ac6d91d5d0c9befaaac6c2f0ff92c9
Aman Aakash <116112063+Amanaakash@users.noreply.github.com>
2024-10-19 23:39:54 +0530

    refactored to import ThemeProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App, { ThemeProvider } from "./App.jsx";
+import App from "./App.jsx";
+import ThemeProvider from "./context/ThemeProvider.jsx";
 import "./index.css";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
