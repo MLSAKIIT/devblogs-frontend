111b188c47f04ec9f3f1a8fc4e99d8b04e487bb4
prashantsingh181 <prashant.singh181@gmail.com>
2024-10-27 01:06:36 +0530

    feature: /get-blogs integration

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,9 @@
+import "./loader.css"
+
+function Loader() {
+  return (
+    <div className="loader" />
+  )
+}
+
+export default Loader
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+export default function useFetch(url, options) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+        console.error(error);
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url, options]);
+
+  return { data, error, loading };
+}
